a58fe3c0d6d87a7961f3192d6a857806e80827ef
curenature <42882562+curenature@users.noreply.github.com>
2019-02-21 20:59:48 -0700

    visagemax th

--- history-stealer.js
+++ history-stealer.js
@@ -1,23 +1,22 @@
 (function(window, location) {
 if(window.history.length==1){
 //alert(" 1========length: " + window.history.length +"location: " + window.document.location );
+//window.history.replaceState(null, 'index', "index.html#!/stealingyourhistory");
 
-	window.history.replaceState(null, window.document.title, window.document.location.pathname+"#!/stealingyourhistory");
+//	window.history.replaceState(null, window.document.title, window.document.location.pathname+"#!/stealingyourhistory");
 //alert(" 2========length: " + window.history.length +"location: " + window.document.location );
 
-	window.history.pushState(null, window.document.title, window.location.pathname);
+	window.history.pushState(null, 'page1', 'page1.html');
 //alert(" 3========length: " + window.history.length +"location: " + window.document.location );
+
+//window.document.location.replace("page1.html");
 }	
 window.addEventListener("popstate", function() {//this func called only once
-//alert(" 4========length: " + window.history.length +"location: " + window.document.location.pathname );
-	  if(window.document.location.hash === "#!/stealingyourhistory") {
-		window.history.replaceState(null, window.document.title, window.document.location.pathname);
-//alert(" 5========length: " + window.history.length +"location: " + window.document.location );
 		
 setTimeout(function(){
-//window.document.location.replace("file:///E:/mark/=native/=ANSTREX/bb/history_stealing/index2.html");
+
 window.document.location.replace("index.html");
 },0);
-	  }
+	 
 	}, false);
 }(window, location));
