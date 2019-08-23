ed0a9fae6522ed2cf7cf9dfb4b2cc4092634671d
curenature <42882562+curenature@users.noreply.github.com>
2019-08-23 17:52:31 -0600

    08232019-3

--- files/trivia-crack-inspired---spinning-wheel/README.markdown
+++
@@ -1,4 +0,0 @@
-# Trivia Crack Inspired - Spinning Wheel
- _A Pen created at CodePen.io. Original URL: [https://codepen.io/AndreCortellini/pen/vERwmL](https://codepen.io/AndreCortellini/pen/vERwmL).
-
- 
--- geolocation.php
+++
@@ -1,3 +0,0 @@
-<!--?php $referringDomain = $_SERVER['HTTP_REFERER']; $myDomain = 'domain.com'; if ( preg_match("($myDomain)i", $referringDomain, $matches) == false ) { //no match - output redirect javascript ?-->
-window.top.location.href=’http://take-action.net/th/index.php’;
-<?php Die(); } else { // do nothing } ?>
